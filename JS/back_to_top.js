ab767dea45a5e7584ac75f670f92e7c0e0a710e2
David De La Puente Enriquez <Twinkymen@hotmail.com>
2024-04-26 14:11:37 +0200

    Limpiando arbol de archivos

---
+++ JS/back_to_top.js
@@ -0,0 +1,18 @@
+// Cuando el usuario hace scroll hacia abajo 20px desde la parte superior del documento, muestra el botón
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("backToTopBtn").style.display = "none";
+    }
+}
+
+// Cuando el usuario hace clic en el botón, vuelve a la parte superior del documento
+function topFunction() {
+    document.body.scrollTop = 0; // Para Safari
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+}
